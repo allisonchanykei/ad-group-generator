031098b7281648f81407705994075d2c88f72341
Allison Chan <allisonchanykei@gmail.com>
2019-05-08 17:52:28 -0400

    added Core job title generator

--- src/Code.ts
+++ src/Code.ts
@@ -8,9 +8,19 @@ import { CoreTransactional } from './keywordsGenerator/coreTransactional';
 import { AdjectiveCoreTransactional } from './keywordsGenerator/adjectiveCoreTransactional';
 import { CoreServiceProviderTransactional } from './keywordsGenerator/coreServiceProviderTransactional';
 import { AdjectiveCoreServiceProvider } from './keywordsGenerator/adjectiveCoreServiceProvider';
+import { CoreJobTitle } from './keywordsGenerator/coreJobTitle';
 
 function myFunction() {
     const source: ISource = new MockSource();
+    let adGroups: AdGroup[] = [];
+
+    // adGroups = adGroups.concat(getCoreRelatedAdGroups(source));
+    adGroups = adGroups.concat(getCoreJobTitleRelatedAdGroups(source));
+
+    return;
+}
+
+function getCoreRelatedAdGroups(source: ISource): AdGroup[] {
     const adGroups: AdGroup[] = [];
     source.cores.map(core => {
         adGroups.push(new AdGroup(new Core(core, source)));
@@ -39,5 +49,13 @@ function myFunction() {
             adGroups.push(new AdGroup(new CoreTransactional(core, transactional, source)));
         });
     });
-    return;
+    return adGroups;
+}
+
+function getCoreJobTitleRelatedAdGroups(source: ISource): AdGroup[] {
+    const adGroups: AdGroup[] = [];
+    source.coreJobTitles.map(coreJobTitle => {
+        adGroups.push(new AdGroup(new CoreJobTitle(coreJobTitle, source)));
+    });
+    return adGroups;
 }
---
+++ src/keywordsGenerator/coreJobTitle.ts
@@ -0,0 +1,47 @@
+import { IKeywordsGenerator } from './IKeywordsGenerator';
+import { ISource } from '../sources/ISource';
+import { IKeyword, KeywordType } from '../adGroup';
+
+export class CoreJobTitle implements IKeywordsGenerator {
+    constructor(private adWord: string, private source: ISource) {}
+
+    positive(): IKeyword[] {
+        const word = this.adWord;
+        return [
+            {
+                word,
+                type: KeywordType.phrase
+            },
+            {
+                word,
+                type: KeywordType.exact
+            }
+        ];
+    }
+
+    negative(): IKeyword[] {
+        const negativeKeywords: IKeyword[] = this.source.cores.map(
+            core =>
+                <IKeyword>{
+                    word: core,
+                    type: KeywordType.exact
+                }
+        );
+
+        const negativePhrases = this.source.adjectives.concat(
+            this.source.coreExpansions,
+            this.source.serviceProviders,
+            this.source.transactionals
+        );
+
+        const remainingNegativeKeywords: IKeyword[] = negativePhrases.map(
+            word =>
+                <IKeyword>{
+                    word,
+                    type: KeywordType.phrase
+                }
+        );
+
+        return negativeKeywords.concat(remainingNegativeKeywords);
+    }
+}
